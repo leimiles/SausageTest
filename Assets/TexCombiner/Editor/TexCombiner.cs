4dec4c2b05b2a3d3492cf3e969b5f19e46208d5b
朱磊 <zhulei@xmfunny.com>
2021-02-23 20:30:26 +0800

    <feature> update

--- Assets/TexCombiner/Editor/TexCombiner.cs
+++ Assets/TexCombiner/Editor/TexCombiner.cs
@@ -19,7 +19,7 @@ public class TexCombiner : EditorWindow {
     }
 
     public void Init() {
-        guiContent = new GUIContent("Scene Objects:");
+        guiContent = new GUIContent("");
         sideFrame = new Rect(10, 10, 200, 500);
         topFrame = new Rect(220, 10, 500, 150);
         mainFrame = new Rect(220, 170, 500, 500);
@@ -35,7 +35,26 @@ public class TexCombiner : EditorWindow {
         mainFrame.height = this.position.height - 180;
         mainFrame.width = this.position.width - 230;
         GUILayout.BeginArea(mainFrame, guiContent, style);
+        GUI.Box(new Rect(5, 20, mainFrame.width - 10, mainFrame.height - 25), "");
         GUILayout.EndArea();
+    }
+
+    Vector2 pos = new Vector2(10, 10);
+    private void DrawSideFrameList() {
+        List<MeshRenderer> meshRenderers = TexItemList.GetSelectionObject();
+        if(meshRenderers == null || meshRenderers.Count < 1) {
+            GUILayout.Label("Nothing");
+            return;
+        }
+        using(var scrollView =  new GUILayout.ScrollViewScope(this.pos)) {
+            this.pos = scrollView.scrollPosition;
+            foreach(MeshRenderer mr in meshRenderers) {
+                GUILayout.BeginHorizontal();
+                GUILayout.Toggle(false, "");
+                GUILayout.Label(mr.name);
+                GUILayout.EndHorizontal();
+            }
+        }
     }
 
     private GUIStyle GetSideFrameStyle() {
@@ -45,16 +64,28 @@ public class TexCombiner : EditorWindow {
 
     private void DrawTopFrame(GUIStyle style) {
         topFrame.width = this.position.width - 230;
+
         GUILayout.BeginArea(topFrame, guiContent, style);
+        GUI.Box(new Rect(5, 20, topFrame.width - 10, topFrame.height - 25), "");
         GUILayout.EndArea();
     }
 
     private void DrawSideFrame(GUIStyle style) {
         sideFrame.height = this.position.height - 20;
         GUILayout.BeginArea(sideFrame, guiContent, style);
-        if(GUILayout.Button("Load Objects")) {
-
+        GUI.Box(new Rect(5, 40, 190, sideFrame.height - 45), "");
+        GUILayout.BeginHorizontal();
+        if(GUILayout.Button("Load")) {
+            TexItemList.SetData();
         }
+        if(GUILayout.Button("Add")) {
+            TexItemList.AddData();
+        }
+        if(GUILayout.Button("Remove")) {
+            TexItemList.RemoveFromList(1);
+        }
+        GUILayout.EndHorizontal();
+        DrawSideFrameList();
         GUILayout.EndArea();
     }
 }
---
+++ Assets/TexCombiner/TexItemList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TexItemList {
+    private static List<MeshRenderer> _meshRenderers;
+    public static List<MeshRenderer> GetSelectionObject() {
+        return _meshRenderers;
+    }
+
+    public static void RemoveFromList(int index) {
+
+    }
+
+    public static void AddData() {
+        if(_meshRenderers == null) {
+            return;
+        }
+        foreach(var go in Selection.objects) {
+            MeshRenderer mr = (go as GameObject).GetComponent<MeshRenderer>();
+            if(mr != null) {
+                if(!_meshRenderers.Contains(mr)) {
+                    _meshRenderers.Add(mr);
+                }
+            }
+        }
+    }
+
+    public static void SetData() {
+        if(_meshRenderers != null) {
+            _meshRenderers.Clear();
+        }
+        _meshRenderers = new List<MeshRenderer>();
+        if(!(Selection.objects.Length >= 1)) {
+            return;
+        } else {
+            Debug.Log(Selection.objects[0].name);
+        }
+        foreach(var go in Selection.objects) {
+            MeshRenderer mr = (go as GameObject).GetComponent<MeshRenderer>();
+            if(mr != null) {
+                if(!_meshRenderers.Contains(mr)) {
+                    _meshRenderers.Add(mr);
+                }
+            }
+        }
+
+    }
+}
+
